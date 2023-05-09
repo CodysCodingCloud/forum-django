87b801ced0e39b89c82afa6e829ec3c95b608ecc
cody <adwingsx@aim.com>
2023-05-09 17:48:22 -0400

    render static 3

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 # pip install
 pip install -r requirements.txt
 
-PRODUCTION=True python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
